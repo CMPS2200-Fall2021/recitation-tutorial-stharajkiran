05fc97829c7fd8347a04386dc8e14bb47ad086d9
kiran shrestha <kiranshrestha2069@gmail.com>
2021-08-26 06:15:42 -0500

    made changes to test one function

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # this is my first change
 def sum_of_squares(a):
-	pass
+    sum = 0
+    for num in a:
+        square = num ** 2
+        sum += square
+    return sum
 
 def test_one():
     assert sum_of_squares([1,2,3]) == 14
