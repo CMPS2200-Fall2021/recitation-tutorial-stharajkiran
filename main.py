2849778f89a5f11c00f6398b076c9b5f1be55c51
kiran shrestha <kiranshrestha2069@gmail.com>
2021-08-26 10:13:46 -0500

    added the function

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-# first change
-
+# this is my first change
 def sum_of_squares(a):
-	pass
+    sum = 0
+    for num in a:
+        square = num ** 2
+        sum += square
+    return sum
 
 def test_one():
     assert sum_of_squares([1,2,3]) == 14
+    
+test_one() 
