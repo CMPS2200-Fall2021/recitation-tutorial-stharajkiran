49110275b97cb9874ee7c18d82706332d0acf4be
kiran shrestha <kiranshrestha2069@gmail.com>
2021-08-26 10:08:20 -0500

    added a comment

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-# this is my first change
+# first change
+
 def sum_of_squares(a):
-    sum = 0
-    for num in a:
-        square = num ** 2
-        sum += square
-    return sum
+	pass
 
 def test_one():
     assert sum_of_squares([1,2,3]) == 14
-    
-test_one()
